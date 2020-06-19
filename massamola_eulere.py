24c1ed5fc2dd213054599327b32ce727232f5b01
Juliana Guimarães <jufarias13@gmail.com>
2020-06-19 10:25:09 -0300

    + plot energia

--- massamola_eulere.py
+++ massamola_eulere.py
@@ -9,16 +9,31 @@ def f2(v, x):  # dv/dt = - w02 * x
     f = - w02 * x
     return f
 
+def Ec(v):
+    E = (1/2) * (m * (v**2))
+    return E
+
+def Ep(x):
+    E = (1/2) * (k * (x**2))
+    return E
+
 w02 = 3
 tmax = 50
 h = .01
 t0 = 0
 x0 = 2
 v0 = 1
+m = 1
+k = 3
+
+E0 = Ec(v0) + Ep(x0)
+
+print(E0)
 
 t = []
 x = []
 v = []
+E = []
 
 t_n = t0
 x_n = x0
@@ -31,6 +46,8 @@ while (t_n <= tmax):
     t.append(t_n)
     v.append(v_n)
     x.append(x_n)
+    Et = Ec(v_n) + Ep(x_n)
+    E.append((Et - E0)/E0)
     x_n = xnovo
     v_n = vnovo
 
@@ -50,8 +67,20 @@ plt.show()
 xlabel = "posição"
 ylabel = "velocidade"
 
+plt.title(titulo)
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.scatter(x, v, s=.5)
 plt.savefig(arquivo2)
+plt.show()
+
+xlabel = "tempo"
+ylabel = "(E(t) - E0)/E0"
+titulo = "energia x t"
+
+plt.title(titulo)
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.plot(t, E, label = "Euler")
+#plt.savefig(arquivo)
 plt.show()
